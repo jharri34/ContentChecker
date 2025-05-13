7085d60f5cb8507730bd77a3fd7e74d31e5900d0
jharri34 <harrisjayjamall@gmail.com>
2025-05-12 21:23:25 -0400

    simiplifing to see if basic app works

--- src/contentchecker/streamlit_app.py
+++ src/contentchecker/streamlit_app.py
@@ -1,7 +1,7 @@
-import opennsfw2 as n2
+# import opennsfw2 as n2
 import streamlit as st
-from PIL import Image
-import io
+# from PIL import Image
+# import io
 
 
 
@@ -27,17 +27,17 @@ def main():
             st.error("Unsupported file type: {}".format(file_extension))
             return    
         image_data = uploaded_file.read()
-        image = Image.open(io.BytesIO(image_data))
-        st.image(image, caption="Uploaded Image")
-        try:
-            nsfw_probability = n2.predict_image(image)
-            text = "NSFW Probability: {:.2f}".format(nsfw_probability)
-            st.progress(nsfw_probability, text=text)
-        except Exception as e:
-            st.error("Error processing image: {}".format(e))
-            nsfw_probability = None
+        # image = Image.open(io.BytesIO(image_data))
+        # st.image(image, caption="Uploaded Image")
+        # try:
+        #     nsfw_probability = n2.predict_image(image)
+        #     text = "NSFW Probability: {:.2f}".format(nsfw_probability)
+        #     st.progress(nsfw_probability, text=text)
+        # except Exception as e:
+        #     st.error("Error processing image: {}".format(e))
+        #     nsfw_probability = None
         # st.image(uploaded_file, caption=None)
-        st.write("nsfw_probability", nsfw_probability)
+        # st.write("nsfw_probability", nsfw_probability)
     
 if __name__=="__main__":
     main()
